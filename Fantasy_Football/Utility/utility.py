3cf2dd4b37efee31d4b4e321a4d5164c3dfbd97d
Trever <tkalexander@gmail.com>
2024-10-09 00:31:54 -0500

    added get_stats function to helper file

--- Fantasy_Football/Utility/utility.py
+++ Fantasy_Football/Utility/utility.py
@@ -1,86 +1,3 @@
-import sys
-sys.path.append("/Users/treveralexander/Library/CloudStorage/OneDrive-EY/Personal/DE Project/Fantasy_Football")
-import Fantasy_Football.Helper.extractor as helper
-
-
-def get_stats(category, year, position=None):
-
-    """
-    Retrieves player or team statistics based on the specified category, year, and optional position.
-
-      Args (get_player_category_stats):
-        category (str): The category of player statistics you want to retrieve. Choose from the following options:
-            - "passing"
-            - "rushing"
-            - "receiving"
-            - "fumbles"
-            - "tackles"
-            - "interceptions"
-            - "field-goals"
-            - "kickoffs"
-            - "kickoff-returns"
-            - "punts"
-            - "punt-returns"
-
-      Args (get_team_category_stats):
-        category (str): The category of player statistics you want to retrieve. Choose from the following options:
-            - Offense: "passing", "rushing", "receiving", "scoring", "downs"
-            - Defense: "passing", "rushing", "receiving", "scoring", "tackles", "downs", "fumbles", "interceptions"
-            - Special Teams: "field-goals", "scoring", "kickoffs", "kickoff-returns", "punts", "punt-returns"
-        position (str): The position group for which you want to retrieve statistics. Choose from:
-            - "offense"
-            - "defense"
-            - "special-teams"
-     Args:
-        Year (1970 - Current Year)
-
-    Returns:
-        A tuple containing the data and headers for player stats.
-
-    Raises:
-        ValueError: If an invalid Args is provided.
-    """
-    
-
-    if position:
-        team_stats = helper.get_team_category_stats(category, position)
-        url = f'https://www.nfl.com/stats/team-stats/{position}/{team_stats}/{year}/reg/all'
-        data, headers = helper.get_data(url)
-    else:     
-        sort_by = helper.get_player_category_stats(category)
-        url = f'https://www.nfl.com/stats/player-stats/category/{category}/{year}/reg/all/{sort_by}/desc'
-        data, headers = helper.get_data(url)
-        
-    return data, headers
-
-def write_json(path,df):
-    # Directory path
-    json_path = path
-
-    # Create the Delta table
-    df.write_json(json_path,row_oriented=True)
-
-    print(f"json file has successfully been created in: {path}")
-
-def write_delta(path,df):
-    # Directory path
-    delta_path = path
-
-    # Create the Delta table
-    df.write_delta(delta_path)
-
-    print(f"delta file has successfully been created in: {path}")  
-
-def write_csv(path,df):
-    # Directory path
-    csv_path = path
-
-    # Create the Delta table
-    df.write_csv(csv_path)
-
-    print(f"csv file has successfully been created in: {path}")
-
-
 def get_current_year():
     from datetime import datetime
     current_year = datetime.now().year
@@ -106,6 +23,33 @@ def get_end_time():
     import time
     start_time = time.time()
     return start_time
+
+def write_json(path,df):
+    # Directory path
+    json_path = path
+
+    # Create the Delta table
+    df.write_json(json_path)
+
+    print(f"json file has successfully been created in: {path}")
+
+def write_delta(path,df):
+    # Directory path
+    delta_path = path
+
+    # Create the Delta table
+    df.write_delta(delta_path)
+
+    print(f"delta file has successfully been created in: {path}")  
+
+def write_csv(path,df):
+    # Directory path
+    csv_path = path
+
+    # Create the Delta table
+    df.write_csv(csv_path)
+
+    print(f"csv file has successfully been created in: {path}")
 
 
 def get_valid_player_categories(category):
@@ -189,3 +133,129 @@ def current_nfl_regular_season():
   else:
     # we are now out of the regular season
     return int(today.year)
+
+
+def get_player_category_stats(category):
+    """
+    Determines the appropriate sort_by value based on the provided category for individual player statistics.
+
+    Args:
+        category (str): The category of player statistics you want to retrieve. Choose from the following options:
+            - "passing"
+            - "rushing"
+            - "receiving"
+            - "fumbles"
+            - "tackles"
+            - "interceptions"
+            - "field-goals"
+            - "kickoffs"
+            - "kickoff-returns"
+            - "punts"
+            - "punt-returns"
+
+    Returns:
+        A tuple containing the sort_by value.
+
+    """
+
+    category_sort_by_map = {
+        "passing": "passingyards",
+        "rushing": "rushingyards",
+        "receiving": "receivingreceptions",
+        "fumbles": "defensiveforcedfumble",
+        "tackles": "defensivecombinetackles",
+        "interceptions": "defensiveinterceptions",
+        "field-goals": "kickingfgmade",
+        "kickoffs": "kickofftotal",
+        "kickoff-returns": "kickreturnsaverageyards",
+        "punts": "puntingaverageyards",
+        "punt-returns": "puntreturnsaverageyards",
+    }
+    try:
+        sort_by = category_sort_by_map[category]
+        return sort_by
+    except KeyError:
+       return "default_sort_by"
+
+def get_team_category_stats(category, position):
+    """
+    Determines the appropriate team_stats value based on the provided category and position.
+
+    Args:
+        category (str): The category of player statistics you want to retrieve. Choose from the following options:
+            - Offense: "passing", "rushing", "receiving", "scoring", "downs"
+            - Defense: "passing", "rushing", "receiving", "scoring", "tackles", "downs", "fumbles", "interceptions"
+            - Special Teams: "field-goals", "scoring", "kickoffs", "kickoff-returns", "punts", "punt-returns"
+        position (str): The position group for which you want to retrieve statistics. Choose from:
+            - "offense"
+            - "defense"
+            - "special-teams"
+
+    Returns:
+        A tuple containing the team_stats value.
+
+    """
+
+    # Documentation for categories and position:
+    #
+    # Categories:
+    #   Offense:
+    #       - passing: Statistics related to passing plays (passing yards, completions, touchdowns).
+    #       - rushing: Statistics related to rushing plays (rushing yards, attempts, touchdowns).
+    #       - receiving: Statistics related to receiving plays (receptions, yards, touchdowns).
+    #       - scoring: Overall scoring statistics (points scored).
+    #       - downs: Down-specific statistics (third-down conversions, other down-related metrics).
+    #   Defense:
+    #       - passing: Defensive statistics against passing plays (interceptions, sacks, yards allowed).
+    #       - rushing: Defensive statistics against rushing plays (tackles for loss, yards allowed, touchdowns allowed).
+    #       - receiving: Defensive statistics against receiving plays (pass breakups, yards allowed).
+    #       - scoring: Defensive scoring statistics (points allowed).
+    #       - tackles: Overall tackling statistics (total tackles, solo tackles, assisted tackles).
+    #       - downs: Down-specific defensive statistics (stopping opponents on third or fourth down).
+    #       - fumbles: Statistics related to fumbles caused or recovered by the defense.
+    #       - interceptions: Statistics related to interceptions made by the defense.
+    #   Special Teams:
+    #       - field-goals: Statistics related to field goals (made field goals, attempts, percentage).
+    #       - scoring: Special teams scoring statistics (points scored from field goals, extra points, returns).
+    #       - kickoffs: Statistics related to kickoffs (touchbacks, average distance, returns allowed).
+    #       - kickoff-returns: Statistics related to kickoff returns (average yards per return, touchdowns).
+    #       - punts: Statistics related to punts (average distance, hang time, punts inside the 20-yard line).
+    #       - punt-returns: Statistics related to punt returns (average yards per return, touchdowns).
+    #
+    # Position:
+    #   - 'offense': For offensive statistics.
+    #   - 'defense': For defensive statistics.
+    #   - 'special-teams': For special teams statistics.
+
+    position_map = {
+        "offense": {
+            "passing": "passing",
+            "rushing": "rushing",
+            "receiving": "receiving",
+            "scoring": "scoring",
+            "downs": "downs",
+        },
+        "defense": {
+            "passing": "passing",
+            "rushing": "rushing",
+            "receiving": "receiving",
+            "scoring": "scoring",
+            "tackles": "tackles",
+            "downs": "downs",
+            "fumbles": "fumbles",
+            "interceptions": "interceptions",
+        },
+        "special-teams": {
+            "field-goals": "field-goals",
+            "scoring": "scoring",
+            "kickoffs": "kickoffs",
+            "kickoff-returns": "kickoff-returns",
+            "punts": "punts",
+            "punt-returns": "punt-returns",
+        },
+    }
+    try:
+        team_stats = position_map[position][category]
+        return team_stats 
+    except KeyError:
+       return "default_team_stats"
